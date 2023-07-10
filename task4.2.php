806f3d4763c38b23a6c59efa67469e81c8a3149d
sahermohamed1 <139110572+sahermohamed1@users.noreply.github.com>
2023-07-10 10:57:47 +0300

    Create task4.2.php

---
+++ task4.2.php
@@ -0,0 +1,8 @@
+<?php
+
+$products = [
+    'prod1' => ['name' => 'aaaa', 'price' => 150, 'clr' => 'red'],
+    'prod2' => ['name' => 'bbbb', 'price' => 250, 'clr' => 'blue'],
+    'prod3' => ['name' => 'cccc', 'price' => 300, 'clr' => 'yellow'],
+    'prod4' => ['name' => 'dddd', 'price' => 500, 'clr' => 'green'],
+];
